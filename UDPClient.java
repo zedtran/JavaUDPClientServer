f18447d68ea3b566970de1050e504dac686a6937
zedtran <dzt0021@gmail.com>
2018-07-08 00:30:09 -0500

    Updated code and Readme

--- UDPClient.java
+++ UDPClient.java
@@ -1,43 +1,76 @@
+//////////////////////////////////////////////////////////////////////////////
+// File:             UDPClient.java
+// Semester:         COMP4320 - SUMMER 2018
+//
+// Authors:          DON TRAN, THADDEUS HATCHER, EVAN MCCARTHY
+// Lecturer's Name:  Dr. ALVIN LIM
+// Course Section:   001
+//
+//////////////////// CREDITS/SOURCES /////////////////////////////////////////
+//
+// Persons:          
+//
+// Online sources:   
+//
+//////////////////////////////////////////////////////////////////////////////
+
 import java.io.*;
 import java.net.*;
 
-class UDPClient {
+/** 
+*
+* DESCRIPTION:  This program demonstrates how to implement a UDP Client Program
+*
+* ALLOCATED/ASSIGNED Port Numbers:     10000, 10001, 10002, 10003
+* Selected Client/Server:              tux055 --> tux065
+*
+*/
+
+public class UDPClient {
   
-   public static void main(String args[]) throws Exception {
+   public static void main(String[] args) throws Exception {
+      if (args.length < 1) {
+            System.out.println("Syntax: UDPClient <port>");
+            return;
+      }
+ 
+        String hostname = InetAddress.getLocalHost().getHostName().trim();
+        int port = Integer.parseInt(args[0]);
       
-      // Create Input Stream
-      BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            
+            // Create Client Socket
+            DatagramSocket clientSocket = new DatagramSocket();
       
-      // Create Client Socket
-      DatagramSocket clientSocket = new DatagramSocket();
-      
-      // Translate hostname to IP address using DNS
-      InetAddress IPAddress = InetAddress.getByName("hostname");
-   
-      byte[] sendData = new byte[256];
-      byte[] receiveData = new byte[256];
-      
-      String sentence = inFromUser.readLine();
-      
-      sendData = sentence.getBytes();
-      
-      
-      // Begin UDP Datagram with data-to-send, length, IP Address, Port
-      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, IPAddress, 10003); 
-      
-      // Send datagram to server
-      clientSocket.send(sendPacket);
-      
-      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-      
-      // Read datagram from server
-      clientSocket.receive(receivePacket);
-      
-      String modifiedSentence = new String(receivePacket.getData());
-      
-      System.out.println("FROM SERVER:" + modifiedSentence);
-      
-      clientSocket.close();
+            // Translate hostname to IP address using DNS
+            InetAddress IPAddress = InetAddress.getByName(hostname);
+            
+            // Begin UDP Datagram with data-to-request, length, IP Address, Port
+            DatagramPacket requestPacket = new DatagramPacket(new byte[1], 1, IPAddress, port);
+            clientSocket.send(requestPacket);
+            System.out.println("\nRequest packet sent to server.\n");
+               
+            // Read datagram from server //
+            // IF using Latin-Lipsum.txt: Byte Count = 29097
+            // IF using Latin-Lipsum.html: Byte Count = 30738
+            byte[] responseData = new byte[29097];
+            DatagramPacket responsePacket = new DatagramPacket(responseData, responseData.length);
+            clientSocket.receive(responsePacket);
+               
+            String loremIpsumString = new String(responsePacket.getData());
+            System.out.println("\nFROM SERVER:" + loremIpsumString + "\n");
+            clientSocket.close();   
+                
+         
+        } catch (SocketTimeoutException ex) {
+            System.out.println("Timeout error: " + ex.getMessage());
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            System.out.println("Client error: " + ex.getMessage());
+            ex.printStackTrace();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        } 
        
    }
 }
--- UDPServer.java
+++ UDPServer.java
@@ -1,42 +1,96 @@
+///////////////////////////////////////////////////////////////////////////////
+// File:             UDPServer.java
+// Semester:         COMP4320 - SUMMER 2018
+//
+// Authors:          DON TRAN, THADDEUS HATCHER, EVAN MCCARTHY
+// Lecturer's Name:  Dr. ALVIN LIM
+// Course Section:   001
+//
+//////////////////// CREDITS/SOURCES //////////////////////////////////////////
+//
+// Persons:          
+//
+// Online sources:   
+//
+//////////////////////////////////////////////////////////////////////////////
+
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
-class UDPServer {
+/** 
+*
+* DESCRIPTION: This program demonstrates how to implement a UDP Server program 
+*
+* ALLOCATED/ASSIGNED Port Numbers:     10000, 10001, 10002, 10003
+* Selected Client/Server:              tux055 --> tux065
+*
+*/
+public class UDPServer {
    
-   public static void main(String args[]) throws Exception {
-                   
-        // Create datagram socket at port: 10003
-      DatagramSocket serverSocket = new DatagramSocket(10003);
+   private DatagramSocket serverSocket;
+   private List<String> loremList = new ArrayList<String>();
+   private Random random;
+ 
+   public UDPServer(int port) throws SocketException {
+        serverSocket = new DatagramSocket(port);
+        random = new Random();
+   }
+   
+   public static void main(String[] args) throws Exception {             
+      if (args.length < 2) {
+            System.out.println("Syntax: UDPServer <file> <port>");
+            return;
+      }
+ 
+      String ipsumFile = args[0];
+      int port = Integer.parseInt(args[1]);
+ 
+      try {
+            UDPServer server = new UDPServer(port);
+            server.loadIpsumFromFile(ipsumFile);
+            server.service();
+      } catch (SocketException ex) {
+            System.out.println("Socket error: " + ex.getMessage());
+      } catch (IOException ex) {
+            System.out.println("I/O error: " + ex.getMessage());
+      }
+   }
                 
-      byte[] receiveData = new byte[256];
-      byte[] sendData = new byte[256];
-       
-      while(true) {
-         
-         // Create space for received datagram
-         DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-         
-         // Receive datagram
-         serverSocket.receive(receivePacket);
-         
-         String sentence = new String(receivePacket.getData());
-         
-         // Get IP Address & Port # of sender
-         InetAddress IPAddress = receivePacket.getAddress();
-         
-         int port = receivePacket.getPort();
-         
-         String capitalizedSentence = sentence.toUpperCase();
-         
-         sendData = capitalizedSentence.getBytes();
-         
-         // Create datagram to send to client
-         DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-         
-         // Write out datagram to socket
-         
-         serverSocket.send(sendPacket);
-        
-      }   
-   } 
+   
+   private void service() throws IOException {
+        while (true) {
+            DatagramPacket requestPacket = new DatagramPacket(new byte[1], 1);
+            serverSocket.receive(requestPacket);
+ 
+            String LoremIpsumString = loremList.toString();
+            byte[] buffer = LoremIpsumString.getBytes();
+ 
+            InetAddress clientAddress = requestPacket.getAddress();
+            int clientPort = requestPacket.getPort();
+ 
+            DatagramPacket responsePacket = new DatagramPacket(buffer, buffer.length, clientAddress, clientPort);
+            serverSocket.send(responsePacket);
+        }
+   }
+   
+   private void loadIpsumFromFile(String ipsumFile) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(ipsumFile));
+        String lorem;
+ 
+        while ((lorem = reader.readLine()) != null) {
+            loremList.add(lorem);
+        }
+ 
+        reader.close();
+   }
+ 
+    
+    /**
+    private String getRandomLoremListString() {
+        int randomIndex = random.nextInt(loremList.size());
+        String randomLoremString = loremList.get(randomIndex);
+        return randomLoremString;
+    }
+    */ 
 }
